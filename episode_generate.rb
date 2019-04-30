9086cc81f4216b71a24967fe72489aa3401a313c
Hugh O'Brien <hughlavelleobrien@gmail.com>
2019-04-30 17:05:24 +0100

    updating for simplecast api v2

--- episode_generate.rb
+++ episode_generate.rb
@@ -10,17 +10,17 @@ require 'date'
 
 API_KEY = ENV["SIMPLECAST"]
 #CAST_ID = ENV["pie"]
-CAST_ID = 2120
+CAST_ID = '7ff2cd28-a418-4f48-96bd-cb9a534cd3db'
 
-uri = URI.parse("https://api.simplecast.com/v1/podcasts/#{CAST_ID}/episodes.json?")
+uri = URI.parse("https://api.simplecast.com/podcasts/#{CAST_ID}/episodes")
 request = Net::HTTP::Get.new(uri)
-request.basic_auth(API_KEY, "")
 
-req_options = {
-  use_ssl: uri.scheme == "https",
+headers = {
+  'Authorization'=>"Bearer #{API_KEY}",
+  use_ssl: uri.scheme == "https"
 }
 
-response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+response = Net::HTTP.start(uri.hostname, uri.port, headers) do |http|
   http.request(request)
 end
 
@@ -29,19 +29,27 @@ counter = 0
 existing_episodes = Dir["./_posts/*"].map {|e| e.gsub(/.\/_posts\/\d{4}-\d{2}-\d{2}-/, '')
                                             .gsub('.md', '')}
 
-JSON.parse(response.body).each do |episode|
-  next unless episode['published']
+JSON.parse(response.body)['collection'].each do |episode|
+  next unless episode['status']['published']
   # check if it exists, we can't do the exact path matching,
   # Simplecast was returning inconsistent dates
   title = episode['title']
+  puts existing_episodes.include? title
   next if existing_episodes.include? title
 
   # generate file name
   date = Date.parse episode['published_at']
   file_name = "./_posts/#{date}-#{title}.md"
 
+  # make a new request for long description
+  uri = URI.parse(episode['href'])
+  request = Net::HTTP::Get.new(uri)
+  ep_response = Net::HTTP.start(uri.hostname, uri.port, headers) do |http|
+    http.request(request)
+  end
+
   corrected_text =
-    episode['long_description']
+    JSON.parse(ep_response.body)['long_description']
     .gsub(/(?<!\[\d\]:)(?<!\[\d\]: )http.*?[\r \n [:blank:]]/) do |m|
     site = m.split('/')[2]
     "[#{site}](#{m})"
@@ -50,10 +58,10 @@ JSON.parse(response.body).each do |episode|
   # write file from template
   File.binwrite(file_name, <<STRING)
 ---
-layout: episode_v2
+layout: episode_v3
 simplecastId: #{episode['id']}
 title: "#{title}"
-sharing_token: "#{episode['sharing_url'].split('/').last}"
+sharing_token: #{episode['id']}
 description: #{episode['description']}
 ---
 
